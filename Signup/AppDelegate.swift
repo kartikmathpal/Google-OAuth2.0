9f5205816df9433f55764adc6b3da312f791b611
Kartik Mathpal <kmathpal684@gmail.com>
2016-04-02 01:53:15 +0530

    my first commit

---
+++ Signup/AppDelegate.swift
@@ -0,0 +1,83 @@
+//
+//  AppDelegate.swift
+//  Signup
+//
+//  Created by Kartik Mathpal on 27/03/16.
+//  Copyright © 2016 Mathpal Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate {
+
+    var window: UIWindow?
+    
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        var configureError : NSError?
+
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        
+        if(configureError != nil){
+            
+            print("We have an error \(configureError)")
+            
+        }
+        GIDSignIn.sharedInstance().delegate = self
+        return true
+    }
+
+    func signIn(signIn : GIDSignIn!, didSignInForUser user : GIDGoogleUser!, withError error : NSError!){
+        
+        if(error != nil){
+            
+            print("Looks like we got a sign in error : \(error)")
+            
+        }
+            
+        else{
+            
+            print("Wow user signed in \(user)")
+            
+        }
+        
+    }
+    
+    func application(application : UIApplication, openURL url : NSURL, sourceApplication : String?, annotation : AnyObject)->Bool{
+        return GIDSignIn.sharedInstance().handleURL(url, sourceApplication : sourceApplication, annotation : annotation)
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
